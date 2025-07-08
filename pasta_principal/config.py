cd3afda6aeb5c895b554433aa952f36bbb22e109
luizcsosilva <luizcarlos.94@live.com>
2025-07-07 21:11:02 -0300

    Criação de função para calcular a definição dos lembretes

--- pasta_principal/config.py
+++ pasta_principal/config.py
@@ -36,3 +36,20 @@ def configuracoes_usuario(litros_str, intervalo_str, inicio_str, final_str):
 
 
 
+
+
+def calcular_lembretes(litros, intervalo, horario_ini, horario_fin):
+    
+    tempo_ativo = horario_fin - horario_ini
+    total_minutos = tempo_ativo.total_seconds() // 60
+
+ 
+    quantidade_lembretes = int(total_minutos // intervalo)
+
+    if quantidade_lembretes == 0:
+        return "Erro: período muito curto para o intervalo escolhido."
+
+ 
+    quantidade_por_lembrete = round(litros / quantidade_lembretes, 2)
+
+    return quantidade_lembretes, quantidade_por_lembrete
