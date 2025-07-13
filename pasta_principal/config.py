bbf7ea290ae639b3fabe88c764aaab8068605ad0
luizcsosilva <luizcarlos.94@live.com>
2025-07-12 23:07:37 -0300

    Criação de funções mostrar notificação, espera, dados do usuario, lembretes e ciclo.

--- pasta_principal/config.py
+++ pasta_principal/config.py
@@ -1,4 +1,13 @@
 from datetime import datetime, timedelta
+from notifypy import Notify
+import time
+
+_usuario = None
+_qtd_por_lembrete = None
+
+
+
+
 
 def configuracoes_usuario(litros_str, intervalo_str, inicio_str, final_str):
 
@@ -37,7 +46,6 @@ def configuracoes_usuario(litros_str, intervalo_str, inicio_str, final_str):
 
 
 
-
 def calcular_lembretes(litros, intervalo, horario_ini, horario_fin):
     
     tempo_ativo = horario_fin - horario_ini
@@ -53,3 +61,42 @@ def calcular_lembretes(litros, intervalo, horario_ini, horario_fin):
     quantidade_por_lembrete = round(litros / quantidade_lembretes, 2)
 
     return quantidade_lembretes, quantidade_por_lembrete
+
+
+
+
+def mostrar_notificacao(titulo, mensagem):
+    notification = Notify()
+    notification.title = titulo
+    notification.message = mensagem
+    notification.send()
+
+
+
+def esperando(hr_ini,ini_apos,janela):
+    agora=datetime.now()
+    if agora>=hr_ini:
+        ini_apos()
+    else:
+        janela.after(1000, lambda: esperando(hr_ini,ini_apos,janela))
+
+
+
+def dados_usuario(usuario, qtd_por_lembrete):
+    global _usuario, _qtd_por_lembrete
+    _usuario = usuario
+    _qtd_por_lembrete = qtd_por_lembrete
+
+
+
+def iniciar_lembretes(lembretes_total,intervalo_min,janela):
+    from config import _usuario, _qtd_por_lembrete
+
+    def ciclo(lembretes_restantes):
+        if lembretes_restantes==0:
+            return
+        mostrar_notificacao("Hora de beber Água", f"olá {_usuario} beba {_qtd_por_lembrete}ml")
+        janela.after((intervalo_min*60000), lambda:ciclo(lembretes_restantes-1))
+
+    ciclo(lembretes_total)
+
